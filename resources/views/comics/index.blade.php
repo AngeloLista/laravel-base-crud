3bc618020c79d354c883c9756b61f377c84fa3db
AngeloLista <abstractplayer@gmail.com>
2022-03-22 18:35:37 +0100

    created comics index and show

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.main')
 
 @section('content')
-    adsd
+    <div>
+        @foreach ($comics as $comic)
+            <ul>
+                <li><strong>Title: </strong>{{$comic->title}}</li>
+                <li><img src="{{ $comic->thumb }}" alt="{{ $comic->title }}"></li>
+                <li><strong>Price: </strong>{{$comic->price}}</li>
+                <li><strong>Series: </strong>{{$comic->series}}</li>
+                <li><strong>Sale Date: </strong>{{$comic->sale_date}}</li>
+                <li><strong>Type: </strong>{{$comic->type}}</li>
+                <li><a href="{{ route('comics.show', $comic->id) }}">Scopri di più</a></li>
+            </ul>
+        @endforeach
+    </div>
 @endsection
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.main')
 
 @section('content')
-    pagina dettaglio
+    <ul>
+        <li>{{ $comic->title }}</li>
+        <li><img src="{{ $comic->thumb }}" alt="{{ $comic->title }}"></li>
+                <li><strong>Description: </strong>{{$comic->description}}</li>
+                <li><strong>Price: </strong>{{$comic->price}}</li>
+                <li><strong>Series: </strong>{{$comic->series}}</li>
+                <li><strong>Sale Date: </strong>{{$comic->sale_date}}</li>
+                <li><strong>Type: </strong>{{$comic->type}}</li>
+                <li><a href="{{ route('comics.index', $comic->id) }}">Torna Indietro</a></li>
+    </ul>
 @endsection
