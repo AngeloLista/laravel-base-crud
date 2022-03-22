f14f799a4d6129c5d2c9d521637a69936edd962f
AngeloLista <abstractplayer@gmail.com>
2022-03-22 17:38:31 +0100

    created comics index and show and installed bootstrap

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Comics</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+</head>
+<body>
+    <h1>
+        aodskoasd
+    </h1>
+    <header>
+        <nav>
+            <a href="">home</a><a href="">comics</a>
+        </nav>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+    
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</body>
+</html>
