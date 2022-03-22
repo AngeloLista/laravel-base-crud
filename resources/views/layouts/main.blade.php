b428ea6586b75cdcce8a57dd1460bef71a81f679
AngeloLista <abstractplayer@gmail.com>
2022-03-23 00:06:26 +0100

    added favicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <link rel="icon" href="https://www.dccomics.com/sites/all/themes/dc_comics_bp/favicon.ico">
     <link rel="stylesheet" href="{{ asset('css/app.css')}}">
 </head>
 <body>
