49bd2bd5d3e0ea1777950a08cfe805a99be6da98
AngeloLista <abstractplayer@gmail.com>
2022-03-23 18:28:42 +0100

    added temporary saving fields in edit and create forms when errors trigger

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -20,38 +20,38 @@
     
             <div class="col-12 mb-3">
                 <label for="title">Title: </label>
-                <input type="text" name="title" id="title" value="{{ $comic->title }}">
+                <input type="text" name="title" id="title" value="{{ old('title', $comic->title) }}">
             </div>
     
             <div class="col-12 mb-3">
                 <div class="label">Description: </div>
                 <textarea class="p-2" type="text" cols="173"
-                rows="5"  name="description" id="description">{{ $comic->description }}</textarea>
+                rows="5"  name="description" id="description">{{ old('description', $comic->description) }}</textarea>
             </div>
     
             <div class="col-3 mb-3">
                 <label for="type">Type: </label>
-                <input type="text" name="type" id="type" value="{{ $comic->type }}">
+                <input type="text" name="type" id="type" value="{{ old('type', $comic->type) }}">
             </div>
     
             <div class="col-3 mb-3">
                 <label for="price">Price: </label>
-                <input type="text" name="price" id="price" value="{{ $comic->price }}">
+                <input type="text" name="price" id="price" value="{{ old('price', $comic->price) }}">
             </div>
     
             <div class="col-3 mb-3">
                 <label for="series">Series: </label>
-                <input type="text" name="series" id="series" value="{{ $comic->series }}">
+                <input type="text" name="series" id="series" value="{{ old('series', $comic->series) }}">
             </div>
     
             <div class="col-3 mb-3">
                 <label for="sale_date">Sale Date: </label>
-                <input type="date" name="sale_date" id="sale_date" value="{{ $comic->sale_date }}">
+                <input type="date" name="sale_date" id="sale_date" value="{{ old('sale_date', $comic->sale_date) }}">
             </div>
     
             <div class="col-12 mb-3">
                 <label for="thumb">Thumb: </label>
-                <input type="text" name="thumb" id="thumb" value="{{ $comic->thumb }}">
+                <input type="text" name="thumb" id="thumb" value="{{ old('thumb', $comic->thumb) }}">
             </div>
     
             <div>
--- resources/views/includes/comics/form.blade.php
+++ resources/views/includes/comics/form.blade.php
@@ -5,38 +5,38 @@
 
         <div class="col-12 mb-3">
             <label for="title">Title: </label>
-            <input type="text" name="title" id="title">
+            <input type="text" name="title" id="title" value="{{ old('title') }}">
         </div>
 
         <div class="col-12 mb-3">
             <div class="label">Description: </div>
             <textarea type="text" cols="173"
-            rows="5"  name="description" id="description"> </textarea>
+            rows="5"  name="description" id="description"> {{ old('description') }}</textarea>
         </div>
 
         <div class="col-3 mb-3">
             <label for="type">Type: </label>
-            <input type="text" name="type" id="type">
+            <input type="text" name="type" id="type" value="{{ old('type') }}">
         </div>
 
         <div class="col-3 mb-3">
             <label for="price">Price: </label>
-            <input type="text" name="price" id="price">
+            <input type="text" name="price" id="price" value="{{ old('price') }}">
         </div>
 
         <div class="col-3 mb-3">
             <label for="series">Series: </label>
-            <input type="text" name="series" id="series">
+            <input type="text" name="series" id="series" value="{{ old('series') }}">
         </div>
 
         <div class="col-3 mb-3">
             <label for="sale_date">Sale Date: </label>
-            <input type="date" name="sale_date" id="sale_date">
+            <input type="date" name="sale_date" id="sale_date" value="{{ old('sale_date') }}">
         </div>
 
         <div class="col-12 mb-3">
             <label for="thumb">Thumb: </label>
-            <input type="text" name="thumb" id="thumb">
+            <input type="text" name="thumb" id="thumb" value="{{ old('thumb') }}">
         </div>
 
         <div>
